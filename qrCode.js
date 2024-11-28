a3b7109362aba6ff6ace2b8d5e3de44fe35da66e
Ivan Martinez <ivan18.im67@gmail.com>
2024-11-28 15:31:44 -0600

    feat(*):adding download feature

--- qrCode.js
+++ qrCode.js
@@ -1,7 +1,24 @@
 const qrImage = document.querySelector('#qr-code-image');
 const qrUrl = localStorage.getItem('qrUrl');
+const downloadButton = document.querySelector('#download');
+const shareButton = document.querySelector('#share');
 
 if(qrUrl) {
     const qr = new QRCode(qrImage);
     qr.makeCode(qrUrl);
+    console.log(qr)
 }
+
+function downloadQrCode(){
+    const qrCode = document.querySelector('#qr-code-image img');
+    if(qrCode) {
+        const downloadInstance = document.createElement('a');
+
+        downloadInstance.href = qrCode.src;
+        downloadInstance.download = 'qrCode';
+        downloadInstance.target = '_blank';
+        downloadInstance.click();
+    }
+}
+
+downloadButton.addEventListener('click', downloadQrCode);
