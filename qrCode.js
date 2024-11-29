c6ca1b567ab4195a66319deab52724af36b810e5
Ivan Martinez <ivan18.im67@gmail.com>
2024-11-29 10:28:28 -0600

    feat(*):adding share quote feature

--- qrCode.js
+++ qrCode.js
@@ -6,7 +6,6 @@ const shareButton = document.querySelector('#share');
 if(qrUrl) {
     const qr = new QRCode(qrImage);
     qr.makeCode(qrUrl);
-    console.log(qr)
 }
 
 function downloadQrCode(){
@@ -20,5 +19,16 @@ function downloadQrCode(){
         downloadInstance.click();
     }
 }
-
+function shareQuote(){
+    if(qrUrl) {
+        navigator.clipboard.writeText(qrUrl);
+        shareButton.style.backgroundColor = 'green';
+        shareButton.setAttribute('value', 'Copiado');
+        setTimeout(() => {
+            shareButton.style.backgroundColor = '#3662E3';
+            shareButton.setAttribute('value', 'Share');
+        },3000)
+    }
+}
 downloadButton.addEventListener('click', downloadQrCode);
+shareButton.addEventListener('click', shareQuote);
